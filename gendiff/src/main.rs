e081283c2573bb19e25e1096bed1a7e4cc8ac583
Alexander Sidorov <sidai@bk.ru>
2023-10-22 00:00:51 +0300

    feat: Compare jsons

--- gendiff/src/main.rs
+++ gendiff/src/main.rs
@@ -1,26 +1,28 @@
 use actix_web::{web, App, HttpServer, HttpResponse, Responder};
-use serde_json::{Value};
-use json_diff::{diff, diff_config, GroupBy, Default};
+use serde_json::Value;
+use difference::Changeset;
 
+fn compare_json(json1: &(Value, Value), json2: &Value) -> String {
+    let json_string1 = serde_json::to_string_pretty(&json1).unwrap();
+    let json_string2 = serde_json::to_string_pretty(&json2).unwrap();
 
-async fn compare_json(data: web::Json<(Value, Value)>) -> impl Responder {
-    let diff_config = diff_config {
-        color: true,
-        group_by: GroupBy::Type,
-        ..Default::default()
-    };
-
-    let diff_result = diff(&data.0, &data.1, &diff_config);
-    let diff_string = serde_json::to_string_pretty(&diff_result).unwrap_or_else(|_| "Unable to serialize diff result.".to_string());
-
-    HttpResponse::Ok().body(diff_string)
+    let changeset = Changeset::new(&json_string1, &json_string2, "\n");
+    changeset.to_string()
 }
 
-fn main() -> std::io::Result<()> {
+// Define a handler for the compare endpoint
+async fn compare_json_handler(data: web::Json<(Value, Value)>) -> impl Responder {
+    let comparison_result = compare_json(&data.0, &data.1);
+    HttpResponse::Ok().body(comparison_result)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/compare", web::post().to(compare_json))
+            .route("/compare", web::post().to(compare_json_handler))
     })
     .bind("127.0.0.1:8080")?
     .run()
+    .await
 }
